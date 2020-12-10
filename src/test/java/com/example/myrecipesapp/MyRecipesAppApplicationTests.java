a79465429bc1889fc60d4bc56878893833adff33
Alex Draev <a.draev@codecoda.com>
2020-12-10 13:47:05 +0200

    Spring Setup

---
+++ src/test/java/com/example/myrecipesapp/MyRecipesAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.myrecipesapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyRecipesAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
