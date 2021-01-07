5c4c075774a753add723550fc8978b8ae5bb0ce1
Alex Draev <a.draev@codecoda.com>
2021-01-07 13:51:23 +0200

    added User model, UserController, UserRepository

--- src/main/java/com/example/myrecipesapp/MyRecipesAppApplication.java
+++ src/main/java/com/example/myrecipesapp/MyRecipesAppApplication.java
@@ -1,7 +1,11 @@
 package com.example.myrecipesapp;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.stream.Stream;
 
 @SpringBootApplication
 public class MyRecipesAppApplication {
@@ -10,4 +14,14 @@ public class MyRecipesAppApplication {
 		SpringApplication.run(MyRecipesAppApplication.class, args);
 	}
 
+	@Bean
+	CommandLineRunner init(UserRepository userRepository) {
+		return args -> {
+			Stream.of("John", "Julie", "Jennifer", "Helen", "Rachel").forEach(name -> {
+				User user = new User(name, name.toLowerCase() + "@domain.com");
+				userRepository.save(user);
+			});
+			userRepository.findAll().forEach(System.out::println);
+		};
+	}
 }
